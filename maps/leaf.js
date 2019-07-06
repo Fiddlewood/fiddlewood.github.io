2ca78d41f3fff593fc18e9aef10eb3a2a84741af
Fiddlewood <52595335+Fiddlewood@users.noreply.github.com>
2019-07-06 18:30:04 +0200

    Update leaf.js
    Fixed a https problem

--- maps/leaf.js
+++ maps/leaf.js
@@ -4,7 +4,7 @@ var map = L.map( 'map', {
   zoom: 8
 })
 
-L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+L.tileLayer( 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
   subdomains: ['a', 'b', 'c']
 }).addTo( map )
